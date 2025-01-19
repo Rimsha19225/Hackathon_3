2d60c4d4eabea45635865c77e1ab53f482e69634
Rimsha Arshad <adeelarshad8900@gmail.com>
2025-01-19 14:46:43 +0500

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+      <div className="w-full bg-black text-[#ffffff]">
+        <div className="w-[90%] h-[38px] flex justify-around items-center mx-auto">
+            <div className="satoshi ml-[3rem] text-center">
+                Sign up and get 20% off to your first order. 
+                <span className=" pl-2 cursor-pointer underline hover:no-underline">
+                    Sign Up Now
+                </span>
+            </div>
+        </div>
+      </div>
+  )
+}
+
+export default Header
