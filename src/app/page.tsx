733b269c73fe7131c2504ba89b777dd02b6bb79e
Rimsha Arshad <adeelarshad8900@gmail.com>
2025-01-28 14:24:51 +0500

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/footer";
 
 
 export default function Home() {
   return (
     <div className="">
-      
+      <h1 className="mt-[6rem] mb-[15rem]">rim</h1>
+     
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-      <div className="w-full bg-black text-[#ffffff]">
+      <div className="w-full bg-black text-[#ffffff] fixed top-0 z-50">
         <div className="w-[90%] h-[30px] sm:h-[38px] flex justify-around items-center mx-auto">
             <div className="satoshi ml-[0.5rem] sm:ml-[2rem] md:ml-[3rem] text-center text-[10px] sm:text-[14px]">
                 Sign up and get 20% off to your first order. 
